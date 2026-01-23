54c6f23e427fe1285f76218d0bc3a86e15ab637a
Karabo Sambo <karabosambo@outlook.com>
2026-01-23 10:01:12 +0200

    chore: add ESLint configuration for Expo project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,5 @@
-import { SplashScreen, Stack } from "expo-router";
-import "./globals.css"
-import { useFonts } from "expo-font";
-import { useEffect } from "react";
-import GlobalProvider from "@/lib/global-provider";
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  const [fontsLoaded] = useFonts({
-    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
-    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
-    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
-    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
-    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
-    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
-  });
-
-  useEffect(() => {
-    if(fontsLoaded){
-      SplashScreen.hideAsync();
-    }
-    
-  }, [fontsLoaded])
-  
-  if(!fontsLoaded) return null;
-
-  return (
-    <GlobalProvider>
-      <Stack screenOptions={{headerShown: false}}/>
-    </GlobalProvider>
-  );
+  return <Stack />;
 }
